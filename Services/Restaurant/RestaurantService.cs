c5100ddeebc3a81916b8b9d437e0d607b91cabda
Kristian Beall <beallkristian@gmail.com>
2022-09-28 19:01:57 -0400

    GetAllRestaurantsAsync() added to RestaurantService.

--- Services/Restaurant/RestaurantService.cs
+++ Services/Restaurant/RestaurantService.cs
@@ -2,17 +2,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using RestaurantRaterMVC.Data;
+using RestaurantRaterMVC.Models.Restaurant;
 
 namespace RestaurantRaterMVC.Services.Restaurant
 {
     public class RestaurantService
     {
-        private RestaurantDbContext _context;
+        private readonly RestaurantDbContext _context;
         public RestaurantService(RestaurantDbContext context)
         {
             _context = context;
         }
         
+        public async Task<List<RestaurantListItem>> GetAllRestaurantsAsync()
+        {
+            List<RestaurantListItem> restaurants = await _context.Restaurants
+            .Include(r => r.Ratings)
+            .Select(r => new RestaurantListItem()
+            {
+                Id = r.Id,
+                Name = r.Name,
+                Score = r.Score,
+            }).ToListAsync();
+            return restaurants;
+        }
     }
 }
