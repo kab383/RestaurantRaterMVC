8d8229840cc69c0447dd46eae23a515a633272ae
Kristian Beall <beallkristian@gmail.com>
2022-09-30 15:16:50 -0400

    Added RestaurantCreate model.

--- Models/Restaurant/RestaurantCreate.cs
+++ Models/Restaurant/RestaurantCreate.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,11 @@ namespace RestaurantRaterMVC.Models.Restaurant
 {
     public class RestaurantCreate
     {
-        
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Location { get; set; }
     }
 }
